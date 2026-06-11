c5aec7ee2c18191bdde66ced0a6bb3db9568fb27
siddhesh-ss <siddheshshelar32@gmail.com>
2026-06-11 12:28:15 +0000

    Added features : 	- rainbow 	- fill 	- mousedown and mouseenter for paint

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ const submitBtn = document.getElementById("submitBtn");
 const eraser = document.getElementById("eraser");
 const clear = document.getElementById("clear");
 const colorInput = document.getElementById("colorInput");
+const rainbowBtn = document.getElementById("rainbow");
+const fillBtn = document.getElementById("fill");
+const shadowBtn = document.getElementById("shadow");
+let inputText = document.getElementById("inputText");
 
 let inputSize = 16;
 let erase = false;
+let rainbow = false;
+let shadow = false;
 let colorValue = "black";
+let mousedown = false;
+
 function createGrid(n) {
     for(let i = 0 ; i < n ; i++) {
         let row = document.createElement("div");
@@ -17,13 +25,24 @@ function createGrid(n) {
             col.classList.add("col");
 
             col.addEventListener("mouseenter", (e) => {
+                if(!mousedown) return;
                 if(erase) {
                     e.target.style.background = "";
-                    e.target.style.cursor = "grab";
+                    // e.target.style.cursor = "grab";
                 }
                 else {
-                    e.target.style.background = `${colorValue}`;
-                    e.target.style.cursor = "pointer";
+                    if(rainbow) {
+                        let r = Math.ceil(255*Math.random());
+                        let g = Math.ceil(255*Math.random());
+                        let b = Math.ceil(255*Math.random());
+                        e.target.style.background = `rgb(${r}, ${g}, ${b})`;
+                    }
+                    else if(shadow) {
+                        e.target.style.opacity = "0.5";
+                        e.target.style.background = "black";
+                    }
+                    else e.target.style.background = `${colorValue}`;
+                    // e.target.style.cursor = "pointer";
                 }
             });
 
@@ -37,13 +56,51 @@ submitBtn.addEventListener("click", () => {
     inputSize = Number(input.value);
     container.innerHTML = "";
     if(inputSize > 100 || inputSize < 1) alert("Enter between 1 to 100 only");
-    else createGrid(inputSize);
+    else {
+        createGrid(inputSize);
+    }
 });
 
 eraser.addEventListener("click", () => {
     erase = !erase;
-    if(erase) eraser.style.background = "red";
+    if(erase) {
+        eraser.style.background = "red";
+
+        rainbow = false;
+        rainbowBtn.style.background = "#3b82f6";
+
+        shadow = false;
+        shadowBtn.style.background = "#3b82f6";
+    }
     else eraser.style.background = "#3b82f6";
+});
+
+rainbowBtn.addEventListener("click", () => {
+    rainbow = !rainbow;
+    if(rainbow) {
+        rainbowBtn.style.background = "red" ;
+
+        erase = false;
+        eraser.style.background = "#3b82f6";
+
+        shadow = false;
+        shadowBtn.style.background = "#3b82f6";
+    }
+    else rainbowBtn.style.background = "#3b82f6"
+});
+
+shadowBtn.addEventListener("click", () => {
+    shadow = !shadow;
+    if(shadow) {
+        shadowBtn.style.background = "red" ;
+
+        erase = false;
+        eraser.style.background = "#3b82f6";
+
+        rainbow = false;
+        rainbowBtn.style.background = "#3b82f6";
+    }
+    else shadowBtn.style.background = "#3b82f6";
 });
 
 clear.addEventListener("click", () => {
@@ -54,6 +111,20 @@ clear.addEventListener("click", () => {
 colorInput.addEventListener("input", (e) => {
     colorValue = colorInput.value;
 });
+
+input.addEventListener("input", () => {
+    inputText.textContent = `Grid : ${input.value} x ${input.value}`;
+});
+
+fillBtn.addEventListener("click", () => {
+    let col = document.querySelectorAll(".col");
+    col.forEach(box => {
+        box.style.background = `${colorValue}`;
+    });
+})
+
+document.addEventListener("mousedown", () => mousedown = true);
+document.addEventListener("mouseup", () => mousedown = false);
 // let col = document.querySelectorAll(".col");
 // col.forEach(box => {
 //     box.addEventListener("mouseenter", (e) => {
